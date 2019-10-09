e7c796154e15790b461c7e358f1b252bf7529830
Dan Finlay <dan@danfinlay.com>
2019-10-09 09:24:55 +0900

    Add very simple hacky tx listener

--- examples/new-tx-listener/index.js
+++ examples/new-tx-listener/index.js
@@ -1,17 +1,6 @@
-wallet.onMetaMaskEvent('newUnapprovedTx', handleTx)
+wallet.onNewTx(handleTx)
 
 function handleTx (txMeta) {
   console.log(`Plugin detected txMeta: ${JSON.stringify(txMeta)}`)
 }
-
-wallet.registerRpcMessageHandler(
-  async (originString, requestObject) => {
-    switch (requestObject.method) {
-      case 'ping':
-        return 'pong'
-      default:
-        throw new Error('Method not found.')
-    }
-  }
-)
 
