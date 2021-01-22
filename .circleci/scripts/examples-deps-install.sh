3bfdd88047a39475c05376e30a42921b492f54af
Erik Marks <rekmarks@protonmail.com>
2021-01-21 18:17:31 -0800

    fixup! Fix examples dependency install in CI

--- .circleci/scripts/examples-deps-install.sh
+++ .circleci/scripts/examples-deps-install.sh
@@ -7,6 +7,7 @@ set -o pipefail
 
 cd ./examples
 
+# ref: https://github.com/koalaman/shellcheck/wiki/SC2044
 find . -type d -maxdepth 1 -exec sh -c '
     cd "$1"
     yarn --frozen-lockfile --ignore-scripts
