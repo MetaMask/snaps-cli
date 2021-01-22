5293f3a9a47ebf11f5b3fc285268d24e760fdca2
Erik Marks <rekmarks@protonmail.com>
2021-01-21 18:30:33 -0800

    Fix config, script

--- .circleci/scripts/examples-deps-install.sh
+++ .circleci/scripts/examples-deps-install.sh
@@ -8,7 +8,7 @@ set -o pipefail
 cd ./examples
 
 # ref: https://github.com/koalaman/shellcheck/wiki/SC2044
-find . -type d -maxdepth 1 -exec sh -c '
+find . -mindepth 1 -maxdepth 1 -type d -exec sh -c '
     cd "$1"
     yarn --frozen-lockfile --ignore-scripts
   ' sh {} \;
