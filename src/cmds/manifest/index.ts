ab3c3d65b5556ce35bb175a88f013b27614041d2
AngelaLu <angelfish@g.ucla.edu>
2021-01-27 11:51:45 -0800

    manifest migration

--- src/cmds/manifest/index.ts
+++ src/cmds/manifest/index.ts
@@ -1,17 +1,20 @@
 const builders = require('../../builders');
-const { logError } = require('../../utils');
-const manifest = require('./manifest');
+const { manifest } = require('./manifest');
+import { logError } from '../../utils';
+
+import yargs from "yargs";
+import { Argument } from "../../types/yargs";
 
 module.exports.command = ['manifest', 'm'];
 module.exports.desc = 'Validate project package.json as a Snap manifest';
-module.exports.builder = (yarg) => {
+module.exports.builder = (yarg: yargs.Argv) => {
   yarg
     .option('dist', builders.dist)
     .option('port', builders.port)
     .option('populate', builders.populate);
 };
 
-module.exports.handler = async (argv) => {
+module.exports.handler = async (argv: Argument) => {
   try {
     await manifest(argv);
   } catch (err) {
