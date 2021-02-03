faff8fccbbfd26e9d073a354c3f7f9860c7d6045
Erik Marks <rekmarks@protonmail.com>
2021-02-03 13:55:02 -0800

    Reorganize utils.test

--- test/utils.test.js
+++ test/utils.test.js
@@ -8,68 +8,78 @@ const setSuppressWarnings = (bool) => {
   global.snaps.suppressWarnings = bool;
 };
 
-describe('SnapsCLI utility functions', () => {
+describe('utils', () => {
 
-  it('trims a given path string', () => {
-    expect(trimPathString('./hello')).toStrictEqual('hello');
-    expect(trimPathString('hello')).toStrictEqual('hello');
-    expect(trimPathString('hello/')).toStrictEqual('hello');
-    expect(trimPathString('')).toStrictEqual('');
-    expect(trimPathString('hello////')).toStrictEqual('hello');
-    expect(trimPathString('../hello')).toStrictEqual('hello');
+  describe('trimPathString', () => {
+    it('trims a given path string', () => {
+      expect(trimPathString('./hello')).toStrictEqual('hello');
+      expect(trimPathString('hello')).toStrictEqual('hello');
+      expect(trimPathString('hello/')).toStrictEqual('hello');
+      expect(trimPathString('')).toStrictEqual('');
+      expect(trimPathString('hello////')).toStrictEqual('hello');
+      expect(trimPathString('../hello')).toStrictEqual('hello');
+    });
   });
 
-  it('logs an error message to console', () => {
+  describe('logError', () => {
+    it('logs an error message to console', () => {
 
-    setVerboseErrors(true);
-    jest.spyOn(console, 'error').mockImplementation();
-    logError('custom error message', 'verbose error message');
-    expect(global.console.error).toHaveBeenCalledWith('custom error message');
-    expect(global.console.error).toHaveBeenCalledWith('verbose error message');
+      setVerboseErrors(true);
+      jest.spyOn(console, 'error').mockImplementation();
+      logError('custom error message', 'verbose error message');
+      expect(global.console.error).toHaveBeenCalledWith('custom error message');
+      expect(global.console.error).toHaveBeenCalledWith('verbose error message');
 
-    setVerboseErrors(false);
-    jest.spyOn(console, 'error').mockImplementation();
-    logError('error message');
-    expect(global.console.error).toHaveBeenCalledWith('error message');
+      setVerboseErrors(false);
+      jest.spyOn(console, 'error').mockImplementation();
+      logError('error message');
+      expect(global.console.error).toHaveBeenCalledWith('error message');
 
+    });
   });
 
-  it('logs a warning message to console', () => {
+  describe('logWarning', () => {
+    it('logs a warning message to console', () => {
 
-    setSuppressWarnings(false);
-    setVerboseErrors(true);
-    jest.spyOn(console, 'warn').mockImplementation();
-    jest.spyOn(console, 'error').mockImplementation();
-    logWarning('custom warning message', 'verbose warning message');
-    expect(global.console.warn).toHaveBeenCalledWith('custom warning message');
-    expect(global.console.error).toHaveBeenCalledWith('verbose warning message');
+      setSuppressWarnings(false);
+      setVerboseErrors(true);
+      jest.spyOn(console, 'warn').mockImplementation();
+      jest.spyOn(console, 'error').mockImplementation();
+      logWarning('custom warning message', 'verbose warning message');
+      expect(global.console.warn).toHaveBeenCalledWith('custom warning message');
+      expect(global.console.error).toHaveBeenCalledWith('verbose warning message');
 
+    });
   });
 
-  it('gets the complete out file path', () => {
+  describe('getOutfilePath', () => {
+    it('gets the complete out file path', () => {
 
-    expect(getOutfilePath('./src', 'outDir')).toStrictEqual('src/outDir');
-    expect(getOutfilePath('src', 'outDir')).toStrictEqual('src/outDir');
-    expect(getOutfilePath('src/', './outDir/')).toStrictEqual('src/outDir/');
-    expect(getOutfilePath('src/', '')).toStrictEqual('src/bundle.js');
+      expect(getOutfilePath('./src', 'outDir')).toStrictEqual('src/outDir');
+      expect(getOutfilePath('src', 'outDir')).toStrictEqual('src/outDir');
+      expect(getOutfilePath('src/', './outDir/')).toStrictEqual('src/outDir/');
+      expect(getOutfilePath('src/', '')).toStrictEqual('src/bundle.js');
+    });
   });
 
-  it('ensures outfile name is just a js file name', () => {
+  describe('validateOutfileName', () => {
+    it('ensures outfile name is just a js file name', () => {
 
-    expect(() => {
-      validateOutfileName('file.ts');
-    }).toThrow('Invalid outfile name: file.ts');
+      expect(() => {
+        validateOutfileName('file.ts');
+      }).toThrow('Invalid outfile name: file.ts');
 
-    expect(() => {
-      validateOutfileName('/');
-    }).toThrow('Invalid outfile name: /');
+      expect(() => {
+        validateOutfileName('/');
+      }).toThrow('Invalid outfile name: /');
 
-    expect(() => {
-      validateOutfileName('');
-    }).toThrow('Invalid outfile name: ');
+      expect(() => {
+        validateOutfileName('');
+      }).toThrow('Invalid outfile name: ');
 
-    // expect(validateOutfileName('file.js')).toStrictEqual(true)
-    // expect(validateOutfileName('./src/file')).toStrictEqual(true)
+      // expect(validateOutfileName('file.js')).toStrictEqual(true)
+      // expect(validateOutfileName('./src/file')).toStrictEqual(true)
 
+    });
   });
 });
