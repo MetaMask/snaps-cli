71034fd35b10b2ea909e68c1bd9f4e2ab3422bc5
AngelaLu <angelfish@g.ucla.edu>
2021-02-04 12:58:14 -0800

    my first unsuccessful attempt at mocking the file system

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,48 +1,47 @@
-const { cli } = require('../dist/src/cli');
-const { default: commands } = require('../dist/src/cmds');
+// const { cli } = require('../dist/src/cli');
+// const { default: commands } = require('../dist/src/cmds');
 
-/**
- * Gets a mock argv array for the cli function.
- */
-const getMockArgv = (...args) => ['', '', ...args];
+// /**
+//  * Gets a mock argv array for the cli function.
+//  */
+// const getMockArgv = (...args) => ['', '', ...args];
 
-/**
- * Gets an array of mock yargs command object, and a map object of command
- * names to their mock handlers.
- */
-const getMockCommands = () => {
-  const mockCommands = [];
-  const mockHandlerMap = {};
+// /**
+//  * Gets an array of mock yargs command object, and a map object of command
+//  * names to their mock handlers.
+//  */
+// const getMockCommands = () => {
+//   const mockCommands = [];
+//   const mockHandlerMap = {};
 
-  commands.forEach((commandObj) => {
-    const mockCommandObj = {
-      ...commandObj,
-      handler: jest.fn(),
-    };
-    mockCommands.push(mockCommandObj);
-    mockHandlerMap[commandObj.command[0]] = mockCommandObj.handler;
-  });
+//   commands.forEach((commandObj) => {
+//     const mockCommandObj = {
+//       ...commandObj,
+//       handler: jest.fn(),
+//     };
+//     mockCommands.push(mockCommandObj);
+//     mockHandlerMap[commandObj.command[0]] = mockCommandObj.handler;
+//   });
 
-  return [mockCommands, mockHandlerMap];
-};
+//   return [mockCommands, mockHandlerMap];
+// };
 
+// describe('command line interface', () => {
 
-describe('command line interface', () => {
+//   let mockCommands;
+//   let mockHandlerMap;
 
-  let mockCommands;
-  let mockHandlerMap;
+//   beforeEach(() => {
+//     [mockCommands, mockHandlerMap] = getMockCommands();
+//   });
 
-  beforeEach(() => {
-    [mockCommands, mockHandlerMap] = getMockCommands();
-  });
-
-  describe('build', () => {
-    it('should work', () => {
-      cli(
-        getMockArgv('build'),
-        mockCommands,
-      );
-      expect(mockHandlerMap.build).toHaveBeenCalledWith('build');
-    });
-  });
-});
+//   describe('build', () => {
+//     it('should work', () => {
+//       cli(
+//         getMockArgv('build'),
+//         mockCommands,
+//       );
+//       expect(mockHandlerMap.build).toHaveBeenCalledWith('build');
+//     });
+//   });
+// });
