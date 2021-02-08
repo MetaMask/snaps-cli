5335e3630dfb02093b9d191cc9e7d58cda236a85
AngelaLu <angelfish@g.ucla.edu>
2021-02-08 15:02:57 -0800

    misc unit tests

--- test/utils/misc.test.js
+++ test/utils/misc.test.js
@@ -1,4 +1,4 @@
-const { trimPathString, logError, logWarning } = require('../../dist/src/utils');
+const { trimPathString, logError, logWarning, assignGlobals } = require('../../dist/src/utils/misc');
 
 describe('misc', () => {
 
@@ -6,6 +6,55 @@ describe('misc', () => {
     verboseErrors: false,
     suppressWarnings: false,
     isWatching: false,
+  };
+
+  // this is the yargs object created with cli command: mm-snap init
+  const defaultArgv = {
+    _: ['init'],
+    verboseErrors: false,
+    v: false,
+    verbose: false,
+    'verbose-errors': false,
+    suppressWarnings: false,
+    w: false,
+    'suppress-warnings': false,
+    src: 'index.js',
+    s: 'index.js',
+    dist: 'dist',
+    d: 'dist',
+    outfileName: 'bundle.js',
+    n: 'bundle.js',
+    'outfile-name': 'bundle.js',
+    port: 8081,
+    p: 8081,
+    '$0': '/usr/local/bin/mm-snap',
+  };
+
+  // this is the yargs object created with cli command: mm-snap watch -v -w
+  const exampleArgv = {
+    _: ['watch'],
+    v: true,
+    verboseErrors: true,
+    verbose: true,
+    'verbose-errors': true,
+    w: true,
+    suppressWarnings: true,
+    'suppress-warnings': true,
+    src: 'index.js',
+    s: 'index.js',
+    dist: 'dist',
+    d: 'dist',
+    outfileName: 'bundle.js',
+    n: 'bundle.js',
+    'outfile-name': 'bundle.js',
+    sourceMaps: false,
+    'source-maps': false,
+    environment: 'worker',
+    env: 'worker',
+    stripComments: false,
+    strip: false,
+    'strip-comments': false,
+    '$0': '/usr/local/bin/mm-snap',
   };
 
   const setVerboseErrors = (bool) => {
@@ -31,17 +80,24 @@ describe('misc', () => {
     delete global.snaps;
   });
 
-  describe('trimPathString', () => {
-    it('trims a given path string', () => {
-      expect(trimPathString('./hello')).toStrictEqual('hello');
-      expect(trimPathString('hello')).toStrictEqual('hello');
-      expect(trimPathString('hello/')).toStrictEqual('hello');
-      expect(trimPathString('')).toStrictEqual('');
-      expect(trimPathString('hello////')).toStrictEqual('hello');
-      expect(trimPathString('../hello')).toStrictEqual('hello');
-      expect(trimPathString('//////hello')).toStrictEqual('hello');
+  describe('assignGlobals', () => {
+    it('sets global variables correctly', () => {
+      assignGlobals(exampleArgv);
+      // expect(global.snaps.isWatching).toStrictEqual(true);
+      expect(global.snaps.verboseErrors).toStrictEqual(true);
+      expect(global.snaps.suppressWarnings).toStrictEqual(true);
+    });
+
+    it('doesnt set global variables incorrectly', () => {
+      assignGlobals(defaultArgv);
+      // expect(global.snaps.isWatching).toStrictEqual(false);
+      expect(global.snaps.verboseErrors).toStrictEqual(false);
+      expect(global.snaps.suppressWarnings).toStrictEqual(false);
     });
   });
+
+  // describe('sanitizeInputs', () => {
+  // });
 
   describe('logError', () => {
     it('logs an error message to console', () => {
@@ -69,6 +125,18 @@ describe('misc', () => {
       logWarning('custom warning message', 'verbose warning message');
       expect(global.console.warn).toHaveBeenCalledWith('custom warning message');
       expect(global.console.error).toHaveBeenCalledWith('verbose warning message');
+    });
+  });
+
+  describe('trimPathString', () => {
+    it('trims a given path string', () => {
+      expect(trimPathString('./hello')).toStrictEqual('hello');
+      expect(trimPathString('hello')).toStrictEqual('hello');
+      expect(trimPathString('hello/')).toStrictEqual('hello');
+      expect(trimPathString('')).toStrictEqual('');
+      expect(trimPathString('hello////')).toStrictEqual('hello');
+      expect(trimPathString('../hello')).toStrictEqual('hello');
+      expect(trimPathString('//////hello')).toStrictEqual('hello');
     });
   });
 
