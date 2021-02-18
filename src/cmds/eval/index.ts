794a9e1995fa71219f638fc35bb03e1c31c44d54
AngelaLu <angelfish@g.ucla.edu>
2021-02-18 11:08:59 -0800

    beginning eval

--- src/cmds/eval/index.ts
+++ src/cmds/eval/index.ts
@@ -1,9 +1,7 @@
-import pathUtils from 'path';
-import { Worker } from 'worker_threads';
 import yargs from 'yargs';
 import builders from '../../builders';
-import { logError, validateFilePath } from '../../utils';
 import { YargsArgs } from '../../types/yargs';
+import { snapEval } from './evalWorker';
 
 module.exports.command = ['eval', 'e'];
 module.exports.desc = 'Attempt to evaluate Snap bundle in SES';
@@ -12,40 +10,3 @@ module.exports.builder = (yarg: yargs.Argv) => {
     .option('bundle', builders.bundle);
 };
 module.exports.handler = (argv: YargsArgs) => snapEval(argv);
-
-export async function snapEval(argv: YargsArgs): Promise<boolean> {
-  const { bundle: bundlePath } = argv;
-  await validateFilePath(bundlePath as string);
-  try {
-    // TODO: When supporting multiple environments, evaluate them here.
-    await workerEval(bundlePath as string);
-    console.log(`Eval Success: evaluated '${bundlePath}' in SES!`);
-    return true;
-  } catch (err) {
-    logError(`Snap evaluation error: ${err.message}`, err);
-    process.exit(1);
-  }
-}
-
-function workerEval(bundlePath: string): Promise<null> {
-  return new Promise((resolve, _reject) => {
-    new Worker(getEvalWorkerPath())
-      .on('exit', (exitCode: number) => {
-        if (exitCode === 0) {
-          resolve(null);
-        } else {
-          throw new Error(`Worker exited abnormally! Code: ${exitCode}`);
-        }
-      })
-      .postMessage({
-        pluginFilePath: bundlePath,
-      });
-  });
-}
-
-/**
- * @returns {string} The path to the eval worker file.
- */
-function getEvalWorkerPath(): string {
-  return pathUtils.join(__dirname, 'evalWorker.js');
-}
---
+++ test/cmds/eval/eval.test.js
@@ -0,0 +1,14 @@
+// const { snapEval } = require('../../../dist/src/cmds/eval/evalWorker');
+
+// describe('eval', () => {
+//   describe('snapEval', () => {
+
+//     afterEach(() => {
+//       jest.restoreAllMocks();
+//     });
+
+//     it('snapEval successfully executes and logs to console', async () => {
+//       await expect(manifest(noDistArgv)).rejects.toThrow('Invalid params: must provide \'dist\'');
+//     });
+//   });
+// });
