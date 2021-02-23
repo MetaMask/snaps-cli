5afee00fbc780272ab434da4710846270f6b8462
AngelaLu <angelfish@g.ucla.edu>
2021-02-23 12:30:50 -0800

    build unit tests

--- test/cmds/build/build.test.js
+++ test/cmds/build/build.test.js
@@ -1,50 +1,152 @@
-// const { build } = require('../../../dist/src/cmds/build/bundle');
-// const { utils } = require('../../../dist/src/utils/validate-fs');
+const { build } = require('../../../dist/src/cmds/build/build');
+const evalModule = require('../../../dist/src/cmds/eval/eval');
+const manifestModule = require('../../../dist/src/cmds/manifest/manifest');
+const buildBundle = require('../../../dist/src/cmds/build/bundle');
+const fsUtils = require('../../../dist/src/utils/validate-fs');
 
-// const mockArgv = {
-//   _: ['build'],
-//   verboseErrors: false,
-//   v: false,
-//   'verbose-errors': false,
-//   suppressWarnings: false,
-//   sw: false,
-//   'suppress-warnings': false,
-//   src: 'index.js',
-//   s: 'index.js',
-//   dist: 'dist',
-//   d: 'dist',
-//   outfileName: 'bundle.js',
-//   n: 'bundle.js',
-//   'outfile-name': 'bundle.js',
-//   sourceMaps: false,
-//   'source-maps': false,
-//   stripComments: false,
-//   strip: false,
-//   'strip-comments': false,
-//   port: 8081,
-//   p: 8081,
-//   eval: true,
-//   e: true,
-//   manifest: true,
-//   m: true,
-//   populate: true,
-//   environment: 'worker',
-//   env: 'worker',
-//   '$0': '/usr/local/bin/mm-snap',
-//   bundle: 'dist/bundle.js',
-// };
+describe('build', () => {
+  describe('build', () => {
 
-// describe('bundle and build', () => {
-//   describe('build function', () => {
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
 
-//     afterEach(() => {
-//       jest.restoreAllMocks();
-//     });
+    global.snaps = {
+      verboseErrors: false,
+    };
 
-//     it('snapEval successfully executes and logs to console', async () => {
-//       const validateOutfileNameMock = jest.spyOn(utils, 'validateOutfileName');
-//       await build(mockArgv);
-//       expect(validateOutfileNameMock).toHaveBeenCalledTimes(1);
-//     });
-//   });
-// });
+    it('processes yargs properties correctly', async () => {
+      const mockArgv = {
+        _: ['build'],
+        verboseErrors: false,
+        v: false,
+        'verbose-errors': false,
+        suppressWarnings: false,
+        sw: false,
+        'suppress-warnings': false,
+        src: 'index.js',
+        s: 'index.js',
+        dist: 'dist',
+        d: 'dist',
+        outfileName: 'bundle.js',
+        n: 'bundle.js',
+        'outfile-name': 'bundle.js',
+        sourceMaps: false,
+        'source-maps': false,
+        stripComments: false,
+        strip: false,
+        'strip-comments': false,
+        port: 8081,
+        p: 8081,
+        eval: true,
+        e: true,
+        manifest: true,
+        m: true,
+        populate: true,
+        '$0': '/usr/local/bin/mm-snap',
+      };
+      const outfilePath = `${mockArgv.dist}/${mockArgv.outfileName}`;
+      const validateOutfileNameMock = jest.spyOn(fsUtils, 'validateOutfileName').mockImplementation();
+      const validateFilePathMock = jest.spyOn(fsUtils, 'validateFilePath').mockImplementation();
+      const validateDirPathMock = jest.spyOn(fsUtils, 'validateDirPath').mockImplementation();
+      const bundleMock = jest.spyOn(buildBundle, 'bundle').mockImplementation(() => true);
+      const evalMock = jest.spyOn(evalModule, 'snapEval').mockImplementation();
+      const manifestMock = jest.spyOn(manifestModule, 'manifest').mockImplementation();
+
+      await build(mockArgv);
+      expect(bundleMock).toHaveBeenCalledWith(mockArgv.src, outfilePath, mockArgv);
+      expect(evalMock).toHaveBeenCalledWith({ ...mockArgv, bundle: outfilePath });
+      expect(manifestMock).toHaveBeenCalledWith(mockArgv);
+      expect(validateOutfileNameMock).toHaveBeenCalledTimes(1);
+      expect(validateFilePathMock).toHaveBeenCalledTimes(1);
+      expect(validateDirPathMock).toHaveBeenCalledTimes(1);
+    });
+
+    it('does not call validateOutfileName, snapEval, or manifest without argv parameters', async () => {
+      const mockArgv = {
+        _: ['init'],
+        verboseErrors: false,
+        v: false,
+        'verbose-errors': false,
+        suppressWarnings: false,
+        sw: false,
+        'suppress-warnings': false,
+        src: 'index.js',
+        s: 'index.js',
+        dist: 'dist',
+        d: 'dist',
+        // outfileName: 'bundle.js',
+        n: 'bundle.js',
+        'outfile-name': 'bundle.js',
+        port: 8081,
+        p: 8081,
+        '$0': '/usr/local/bin/mm-snap',
+        // manifest: false,
+        // eval: true,
+      };
+      const outfilePath = `${mockArgv.dist}/bundle.js`;
+      const validateOutfileNameMock = jest.spyOn(fsUtils, 'validateOutfileName').mockImplementation();
+      const validateFilePathMock = jest.spyOn(fsUtils, 'validateFilePath').mockImplementation();
+      const validateDirPathMock = jest.spyOn(fsUtils, 'validateDirPath').mockImplementation();
+      const bundleMock = jest.spyOn(buildBundle, 'bundle').mockImplementation(() => true);
+      const evalMock = jest.spyOn(evalModule, 'snapEval').mockImplementation();
+      const manifestMock = jest.spyOn(manifestModule, 'manifest').mockImplementation();
+      await build(mockArgv);
+      expect(bundleMock).toHaveBeenCalledWith(mockArgv.src, outfilePath, mockArgv);
+      expect(evalMock).not.toHaveBeenCalled();
+      expect(manifestMock).not.toHaveBeenCalled();
+      expect(validateOutfileNameMock).not.toHaveBeenCalled();
+      expect(validateFilePathMock).toHaveBeenCalledTimes(1);
+      expect(validateDirPathMock).toHaveBeenCalledTimes(1);
+    });
+
+    it('does not call eval if bundle fails', async () => {
+      const mockArgv = {
+        _: ['build'],
+        verboseErrors: false,
+        v: false,
+        'verbose-errors': false,
+        suppressWarnings: false,
+        sw: false,
+        'suppress-warnings': false,
+        src: 'index.js',
+        s: 'index.js',
+        dist: 'dist',
+        d: 'dist',
+        outfileName: 'bundle.js',
+        n: 'bundle.js',
+        'outfile-name': 'bundle.js',
+        sourceMaps: false,
+        'source-maps': false,
+        stripComments: false,
+        strip: false,
+        'strip-comments': false,
+        port: 8081,
+        p: 8081,
+        eval: true,
+        e: true,
+        manifest: true,
+        m: true,
+        populate: true,
+        '$0': '/usr/local/bin/mm-snap',
+      };
+      const outfilePath = `${mockArgv.dist}/${mockArgv.outfileName}`;
+      const validateOutfileNameMock = jest.spyOn(fsUtils, 'validateOutfileName').mockImplementation();
+      const validateFilePathMock = jest.spyOn(fsUtils, 'validateFilePath').mockImplementation();
+      const validateDirPathMock = jest.spyOn(fsUtils, 'validateDirPath').mockImplementation();
+      const evalMock = jest.spyOn(evalModule, 'snapEval').mockImplementation();
+      const manifestMock = jest.spyOn(manifestModule, 'manifest').mockImplementation();
+
+      //   main check happens here:
+      const bundleMock = jest.spyOn(buildBundle, 'bundle').mockImplementation();
+      await build(mockArgv);
+      expect(evalMock).not.toHaveBeenCalled();
+
+      expect(bundleMock).toHaveBeenCalledWith(mockArgv.src, outfilePath, mockArgv);
+      expect(manifestMock).toHaveBeenCalledWith(mockArgv);
+      expect(validateOutfileNameMock).toHaveBeenCalledTimes(1);
+      expect(validateFilePathMock).toHaveBeenCalledTimes(1);
+      expect(validateDirPathMock).toHaveBeenCalledTimes(1);
+    });
+  });
+});
