22d35d76fc99ba3a6f6b0177aa305830d8e2d03f
AngelaLu <angelfish@g.ucla.edu>
2021-02-23 00:45:21 -0800

    eval unit tests part 1

--- test/cmds/eval/eval.test.js
+++ test/cmds/eval/eval.test.js
@@ -1,10 +1,13 @@
 const pathUtils = require('path');
-// const { Worker } = require('worker_threads');
+const WorkerThread = require('worker_threads');
+const EventEmitter = require('events');
 const { snapEval } = require('../../../dist/src/cmds/eval/eval');
 const fsUtils = require('../../../dist/src/utils/validate-fs');
 
 describe('eval', () => {
   describe('snapEval', () => {
+
+    let watcherEmitter;
 
     const mockArgv = {
       _: ['eval'],
@@ -19,11 +22,47 @@ describe('eval', () => {
       '$0': '/usr/local/bin/mm-snap',
     };
 
-    afterEach(() => {
-      jest.restoreAllMocks();
+    beforeEach(() => {
+      jest.spyOn(WorkerThread, 'Worker').mockImplementation(() => {
+        watcherEmitter = new EventEmitter();
+        jest.spyOn(watcherEmitter, 'on');
+        return watcherEmitter;
+      });
     });
 
-    it('snapEval successfully executes and logs to console', async () => {
+    afterEach(() => {
+      jest.restoreAllMocks();
+      watcherEmitter = undefined;
+    });
+
+    // cant make successful eval due to postMessage
+    // it('snapEval successfully executes and logs to console', async () => {
+
+    //   global.snaps = {
+    //     verboseErrors: false,
+    //   };
+
+    //   jest.spyOn(console, 'log').mockImplementation();
+    //   jest.spyOn(process, 'exit').mockImplementation(() => undefined);
+    //   const pathMock = jest.spyOn(pathUtils, 'join').mockImplementation();
+    //   const validateFilePathMock = jest.spyOn(fsUtils, 'validateFilePath').mockImplementation(() => true);
+
+    //   await snapEval(mockArgv);
+    //   const finishPromise = new Promise((resolve, _) => {
+    //     watcherEmitter.on('exit', () => {
+    //       resolve();
+    //     });
+    //   });
+    //   watcherEmitter.emit('exit');
+    //   await finishPromise;
+
+    //   expect(validateFilePathMock).toHaveBeenCalledTimes(1);
+    //   expect(pathMock).toHaveBeenCalledTimes(1);
+    //   expect(process.exit).toHaveBeenCalledWith(1);
+    //   expect(global.console.log).toHaveBeenCalledWith('Eval Success: evaluated \'dist/bundle.js\' in SES!');
+    // });
+
+    it('snapEval successfully throws worker and snap eval error', async () => {
 
       global.snaps = {
         verboseErrors: false,
@@ -33,29 +72,22 @@ describe('eval', () => {
       jest.spyOn(process, 'exit').mockImplementation(() => undefined);
       const pathMock = jest.spyOn(pathUtils, 'join').mockImplementation();
       const validateFilePathMock = jest.spyOn(fsUtils, 'validateFilePath').mockImplementation(() => true);
+
       await snapEval(mockArgv);
+      const finishPromise = new Promise((resolve, _) => {
+        watcherEmitter.on('exit', () => {
+          // how to catch this error inside internal function
+          // expect(this).toThrow('Worker exited abnormally! Code: undefined');
+          resolve();
+        });
+      });
+      watcherEmitter.emit('exit');
+      await finishPromise;
+
       expect(validateFilePathMock).toHaveBeenCalledTimes(1);
       expect(pathMock).toHaveBeenCalledTimes(1);
+      expect(global.console.error).toHaveBeenCalledWith('Snap evaluation error: (intermediate value).on(...).postMessage is not a function');
       expect(process.exit).toHaveBeenCalledWith(1);
-      expect(global.console.error).toHaveBeenCalledWith('Snap evaluation error: The "filename" argument must be of type string or an instance of URL. Received undefined');
     });
-
-    // it('snapEval succethnssfully executes and logs to console', async () => {
-
-    //   global.snaps = {
-    //     verboseErrors: false,
-    //   };
-
-    //   jest.spyOn(console, 'log').mockImplementation();
-    //   jest.spyOn(process, 'exit').mockImplementation(() => undefined);
-    //   // const workerMock = jest.spyOn(worker_threads, 'Worker').mockImplementation();
-    //   const pathMock = jest.spyOn(pathUtils, 'join').mockImplementation();
-    //   const validateFilePathMock = jest.spyOn(fsUtils, 'validateFilePath').mockImplementation(() => true);
-    //   await snapEval(mockArgv);
-    //   expect(validateFilePathMock).toHaveBeenCalledTimes(1);
-    //   expect(pathMock).toHaveBeenCalledTimes(1);
-    //   expect(process.exit).not.toHaveBeenCalled();
-    //   // expect(global.console.log).toHaveBeenCalledWith('Eval Success: evaluated \'dist/bundle.js\' in SES!');
-    // });
   });
 });
