2c1fd65e23b0aa9dc32d240f5c49886a3d4f08b4
Angela Lu <angxlfish@gmail.com>
2021-03-12 22:18:24 -0800

    this should work but it doesnt so ill try again tomorrow

--- test/cmds/build/bundle.test.js
+++ test/cmds/build/bundle.test.js
@@ -1,18 +1,10 @@
-// const browserify = require('browserify');
 const { bundle } = require('../../../dist/src/cmds/build/bundle');
 const bundleUtils = require('../../../dist/src/cmds/build/bundleUtils');
 const miscUtils = require('../../../dist/src/utils/misc');
 
-jest.mock('browserify', (_, __) => ({
-  bundle: () => Promise.resolve(true),
-}));
-// jest.mock('browserify');
-// const mockBrowserify = require('browserify');
-// jest.createMockFromModule('browserify');
-// jest.spyOn(brows, 'bundle').mockImplementation();
-
-// jest.mock('init-package-json');
-// const initPackageJson = require('init-package-json');
+jest.mock('browserify');
+// eslint-disable-next-line import/order
+const mockBrowserify = require('browserify');
 
 describe('bundle', () => {
   describe('bundle', () => {
@@ -31,20 +23,9 @@ describe('bundle', () => {
       const mockArgv = {
         sourceMaps: true,
       };
-      //   mockBrowserify.mockImplementation((_, __) => ({
-      //     bundle: () => Promise.resolve(true),
-      //   }));
-      //   jest
-      //     .spyOn(browserify, () => ({
-      //       bundle: () => Promise.resolve(true),
-      //     }));
-      //   const instance = browserify();
-      //   await instance.bundle();
-    //   jest.mock('browserify', (_, __) => ({
-    //     __esModule: false,
-    //     default: jest.fn(),
-    //     bundle: (callback) => callback(Promise.resolve(true)),
-    //   }));
+      mockBrowserify.mockImplementation((_, __) => ({
+        bundle: () => Promise.resolve(true),
+      }));
       const createStreamMock = jest.spyOn(bundleUtils, 'createBundleStream').mockImplementation();
       jest.spyOn(miscUtils, 'writeError').mockImplementation();
       await bundle('src', 'dest', mockArgv);
