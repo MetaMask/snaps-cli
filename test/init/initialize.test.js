2bb4b38d0cc2f12ecd75a0c7a111bed354217a09
Angela Lu <angxlfish@gmail.com>
2021-03-15 15:48:23 -0700

    rework initialize tests

--- test/init/initialize.test.js
+++ test/init/initialize.test.js
@@ -3,9 +3,25 @@ const initUtils = require('../../dist/src/cmds/init/initutils');
 const readlineUtils = require('../../dist/src/utils/readline');
 const miscUtils = require('../../dist/src/utils/misc');
 const { initHandler } = require('../../dist/src/cmds/init/initialize');
+const template = require('../../dist/src/cmds/init/initTemplate.json');
+const { CONFIG_PATHS } = require('../../dist/src/utils/misc');
 
 describe('initialize', () => {
   describe('initHandler', () => {
+
+    beforeEach(() => {
+      const mockWallet = [
+        {
+          bundle: {},
+          initialPermissions: {},
+        },
+        { dist: 'dist', outfileName: 'bundle.js', port: 8081 },
+      ];
+      jest.spyOn(initUtils, 'buildWeb3Wallet').mockImplementation(() => mockWallet);
+      jest.spyOn(initUtils, 'validateEmptyDir').mockImplementation();
+      jest.spyOn(console, 'log').mockImplementation();
+      jest.spyOn(readlineUtils, 'closePrompt').mockImplementation();
+    });
 
     afterEach(() => {
       jest.clearAllMocks();
@@ -13,23 +29,16 @@ describe('initialize', () => {
     });
 
     const mockPackage = {
-      name: 'foo',
-      version: '1.0.0',
-      description: '',
       main: 'index.js',
-      scripts: { test: 'echo "Error: no test specified" && exit 1' },
-      author: '',
-      license: 'ISC',
     };
-
-    const mockArgv = { foo: 'bar' };
-    const mockWallet = [
-      {
+    const mockPackageAndWallet = {
+      main: 'index.js',
+      web3Wallet: {
         bundle: {},
         initialPermissions: {},
       },
-      { dist: 'dist', outfileName: 'bundle.js', port: 8081 },
-    ];
+    };
+    const mockArgv = { foo: 'bar' };
     const expectedReturnValue = {
       foo: 'bar',
       src: 'index.js',
@@ -37,64 +46,55 @@ describe('initialize', () => {
       outfileName: 'bundle.js',
       port: 8081,
     };
+    const newArgs = { dist: 'dist', outfileName: 'bundle.js', port: 8081, src: 'index.js' };
+    const CONFIG_PATH = CONFIG_PATHS[0];
 
     it('function successfully executes under normal conditions', async () => {
-      jest.spyOn(console, 'log').mockImplementation();
       const fsWriteMock = jest.spyOn(fs, 'writeFile').mockImplementation(() => true);
-      const asyncPackageInitMock = jest.spyOn(initUtils, 'asyncPackageInit').mockImplementation(() => mockPackage);
-      const validateEmptyDirMock = jest.spyOn(initUtils, 'validateEmptyDir').mockImplementation();
-      const buildWeb3WalletMock = jest.spyOn(initUtils, 'buildWeb3Wallet').mockImplementation(() => mockWallet);
-      const closePromptMock = jest.spyOn(readlineUtils, 'closePrompt').mockImplementation(() => mockPackage);
+      jest.spyOn(initUtils, 'asyncPackageInit').mockImplementation(() => mockPackage);
+      const closePromptMock = jest.spyOn(readlineUtils, 'closePrompt')
+        .mockImplementation();
       expect(await initHandler(mockArgv)).toStrictEqual(expectedReturnValue);
       expect(global.console.log).toHaveBeenCalledTimes(6);
-      expect(fsWriteMock).toHaveBeenCalledTimes(4);
-      expect(asyncPackageInitMock).toHaveBeenCalledTimes(1);
-      expect(validateEmptyDirMock).toHaveBeenCalledTimes(1);
+      expect(fsWriteMock)
+        .toHaveBeenNthCalledWith(1, 'package.json', `${JSON.stringify(mockPackageAndWallet, null, 2)}\n`);
+      expect(fsWriteMock)
+        .toHaveBeenNthCalledWith(2, mockPackage.main, template.js);
+      expect(fsWriteMock).toHaveBeenNthCalledWith(3, 'index.html', template.html.toString()
+        .replace(/_PORT_/gu, newArgs.port.toString() || mockArgv.port.toString()));
+      expect(fsWriteMock).toHaveBeenNthCalledWith(4, CONFIG_PATH, JSON.stringify(newArgs, null, 2));
       expect(closePromptMock).toHaveBeenCalledTimes(1);
-      expect(buildWeb3WalletMock).toHaveBeenCalledWith(mockArgv);
     });
 
     it('function logs error when writefile is unsuccessful', async () => {
       global.snaps = {
         verboseErrors: false,
       };
-      jest.spyOn(console, 'log').mockImplementation();
       const errorMock = jest.spyOn(miscUtils, 'logError').mockImplementation();
       const fsWriteMock = jest.spyOn(fs, 'writeFile').mockImplementation(() => {
         throw new Error('error message');
       });
       jest.spyOn(process, 'exit').mockImplementation(() => undefined);
-      const asyncPackageInitMock = jest.spyOn(initUtils, 'asyncPackageInit').mockImplementation(() => mockPackage);
-      const validateEmptyDirMock = jest.spyOn(initUtils, 'validateEmptyDir').mockImplementation();
-      const buildWeb3WalletMock = jest.spyOn(initUtils, 'buildWeb3Wallet').mockImplementation(() => mockWallet);
-      const closePromptMock = jest.spyOn(readlineUtils, 'closePrompt').mockImplementation(() => mockPackage);
+      jest.spyOn(initUtils, 'asyncPackageInit').mockImplementation(() => mockPackage);
       expect(await initHandler(mockArgv)).toStrictEqual(expectedReturnValue);
-      expect(global.console.log).toHaveBeenCalledTimes(3);
       expect(errorMock).toHaveBeenCalledTimes(4);
       expect(fsWriteMock).toHaveBeenCalledTimes(4);
-      expect(asyncPackageInitMock).toHaveBeenCalledTimes(1);
-      expect(validateEmptyDirMock).toHaveBeenCalledTimes(1);
-      expect(closePromptMock).toHaveBeenCalledTimes(1);
-      expect(buildWeb3WalletMock).toHaveBeenCalledWith(mockArgv);
       expect(process.exit).toHaveBeenCalledWith(1);
       expect(process.exit).toHaveBeenCalledTimes(3);
     });
 
     it('function does not write to main if undefined', async () => {
-      const mockPackageWithoutMain = { foo: 'bar' };
-      jest.spyOn(console, 'log').mockImplementation();
-      const fsWriteMock = jest.spyOn(fs, 'writeFile').mockImplementation(() => true);
-      const asyncPackageInitMock = jest.spyOn(initUtils, 'asyncPackageInit').mockImplementation(() => mockPackageWithoutMain);
-      const validateEmptyDirMock = jest.spyOn(initUtils, 'validateEmptyDir').mockImplementation();
-      const buildWeb3WalletMock = jest.spyOn(initUtils, 'buildWeb3Wallet').mockImplementation(() => mockWallet);
-      const closePromptMock = jest.spyOn(readlineUtils, 'closePrompt').mockImplementation(() => mockPackageWithoutMain);
-      expect(await initHandler(mockArgv)).toStrictEqual(expectedReturnValue);
-      expect(global.console.log).toHaveBeenCalledTimes(5);
-      expect(fsWriteMock).toHaveBeenCalledTimes(3);
-      expect(asyncPackageInitMock).toHaveBeenCalledTimes(1);
-      expect(validateEmptyDirMock).toHaveBeenCalledTimes(1);
-      expect(closePromptMock).toHaveBeenCalledTimes(1);
-      expect(buildWeb3WalletMock).toHaveBeenCalledWith(mockArgv);
+      const mockUndefinedMain = { foo: 'bar' };
+      const expectedReturn = {
+        foo: 'bar',
+        dist: 'dist',
+        outfileName: 'bundle.js',
+        port: 8081,
+      };
+      jest.spyOn(fs, 'writeFile').mockImplementation(() => true);
+      jest.spyOn(initUtils, 'asyncPackageInit').mockImplementation(() => mockUndefinedMain);
+      jest.spyOn(readlineUtils, 'closePrompt').mockImplementation();
+      expect(await initHandler(mockArgv)).toStrictEqual(expectedReturn);
     });
   });
 });
