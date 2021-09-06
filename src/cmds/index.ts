90271fc07758a635971325749c09c9051f69b145
Erik Marks <rekmarks@protonmail.com>
2021-09-06 16:35:26 -0700

    Fix import / export order in cmds/index.ts

--- src/cmds/index.ts
+++ src/cmds/index.ts
@@ -1,9 +1,9 @@
-import init from './init';
 import build from './build';
 import evaluate from './eval';
+import init from './init';
 import manifest from './manifest';
 import serve from './serve';
 import watch from './watch';
 
-const commands = [init, build, evaluate, manifest, serve, watch];
+const commands = [build, evaluate, init, manifest, serve, watch];
 export default commands;
